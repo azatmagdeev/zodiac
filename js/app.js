5790f5af0b9383ba91ff740339b3152c96377fab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 18:10:07 +0000

    Deploy azatmagdeev/zodiac to github.com/azatmagdeev/zodiac.git:gh-pages

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
 import {findZodiac} from "./lib.js";
 
 const buttonEl = document.getElementById('button');
-console.dir(buttonEl);
 const dateEl = document.getElementById('date');
-
 const elements = document.getElementById('body');
-const element = document.createElement("div",);
 
 buttonEl.addEventListener("click", () => {
-    element.textContent = '';
-    console.dir(dateEl);
+    elements.textContent = '';
     const birthday = new Date(dateEl.value);
     const birthZodiac = new Date(0, birthday.getMonth(), birthday.getDate());
-
-    element.textContent = 'Вы ' + findZodiac(birthZodiac);
-    element.className = 'class';
-    elements.appendChild(element);
-
+    elements.textContent = `Вы ${findZodiac(birthZodiac)}`;
     dateEl.value = '';
-
 });
